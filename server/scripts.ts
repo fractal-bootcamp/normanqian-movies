fc475d810eb2f959b886aba9d499924dd5bd70ab
Norman Qian <normanqian@gmail.com>
2024-06-19 18:11:05 -0400

    prisma, routes, and search

--- server/scripts.ts
+++ server/scripts.ts
@@ -4,8 +4,17 @@ const prisma = new PrismaClient();
 
 async function main() {
   // ... you will write your Prisma Client queries here
-  const allUsers = await prisma.user.findMany();
-  console.log(allUsers);
+
+  /* 
+  const updateMovie = await prisma.movie.update({
+    where: { id: 3 },
+    data: { title: "fatestay", details: "sword of promised victory" },
+  });
+  */
+
+  const getUsers = await prisma.user.findMany();
+  const getMovies = await prisma.movie.findMany();
+  console.log(getUsers, getMovies);
 }
 
 main()
@@ -17,3 +26,19 @@ main()
     await prisma.$disconnect();
     process.exit(1);
   });
+
+/*
+  const createUser = await prisma.user.create({
+    data: {
+      name: "David",
+      favorites: {
+        create: [{ title: "Toy Story", details: "Buzz Lightyear and Woody" }],
+      },
+    },
+  });
+
+  const createMovie = await prisma.movie.create({
+    data: { title: "Toy Story", details: "Buzz Lightyear and Woody" },
+  });
+
+*/
